74951d70657766140cd01ca76def0ae75b5bb24c
Justin Naicker <128274515+Justin-Naicker@users.noreply.github.com>
2023-06-19 16:17:31 -0500

    Create GroundFall.cs

---
+++ GroundFall.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundFall : MonoBehaviour
+{
+
+    bool shouldFall = false;
+    public float fallSpeed = 1f;
+
+    public Player player;
+    public List<Obstacle> obstacles = new List<Obstacle>();
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (shouldFall)
+        {
+            Vector2 pos = transform.position;
+            float fallAmount = fallSpeed * Time.deltaTime;
+            pos.y -= fallAmount;
+
+
+            if (gameObject != null)
+            {
+            }
+
+
+
+            if (player != null)
+            {
+                player.groundHeight -= fallAmount;
+                Vector2 playerPosition = player.transform.position;
+                playerPosition.y -= fallAmount;
+                player.transform.position = playerPosition;
+            }
+
+            transform.position = pos;
+
+            foreach (Obstacle obj in obstacles)
+            {
+
+                if (obj != null)
+                {
+                    Vector2 position = obj.transform.position;
+                    position.y -= fallAmount;
+                    obj.transform.position = position;
+
+                }
+            }
+        }
+
+        else
+        {
+            if (player != null)
+            {
+                shouldFall = true;
+            }
+        }
+    }
+}
